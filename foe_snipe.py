8487d1dd532da4b2e640dfa02ee4f9838c15b176
mostly harmless <59568153+foe-mmr@users.noreply.github.com>
2020-08-12 21:29:18 +0300

    read player ARC bonus, display message about refresh

--- foe_snipe.py
+++ foe_snipe.py
@@ -5,28 +5,38 @@ from operator import getitem
 from TextFormatter import TextFormatter
 
 from tabulate import tabulate
-
-guilds = {}
-guilds_gvg = {}
-
-ARC_bonus = 1.94
-
-toFile = True
+import ast
 
 class EventHandler:
     def __init__(self, tab, world):
         self.tab = tab
         self.world = world
-        self.lastMapOpened = False
+        self.ARC_bonus = 0
+        self.request_cnt = 0
 
+        self.requestIds = []
+
+    
     def handleRequest(self, requestId, **kwargs):
+        requestId = requestId.encode('utf-8')
+
         try:
             request = kwargs.get('request')
             url = request.get('url')
 
             if self.world in url:
+
                 headers = request.get('headers')
-                postdata = request.get('postData', '')
+                postdata = request.get('postData')
+
+                
+                data = ast.literal_eval(postdata)
+
+                rc = data[0].get('requestClass')
+                rm = data[0].get('requestMethod')
+
+                if (rc == "GreatBuildingsService" and rm == "getConstruction") or rc == "StartupService" and rm == "getData":
+                    self.requestIds.append(requestId)
 
             if len(postdata) > 10:
                 postdatalist = json.loads(postdata)
@@ -34,21 +44,12 @@ class EventHandler:
         except Exception as e:
             pass
 
-    def handleResponse(self, requestId, **kwargs):
-        try:
-            response = kwargs.get('response')
-            url = response.get('url')
-
-            if self.dest in url:
-                mt = response.get('mimeType')
-
-            if 'json' in mt:
-                pass
-
-        except Exception as e:
-            pass
-
     def handleLoading(self, requestId, **kwargs):
+        requestId = requestId.encode('utf-8')
+
+        if requestId not in self.requestIds:
+            return False
+
         try:
             body = self.tab.Network.getResponseBody(requestId=requestId)
             responses = json.loads(body.get('body'))
@@ -71,124 +72,159 @@ class EventHandler:
                 hasGreatBuildingsService = True
 
             if hasCityMapService and hasGreatBuildingsService:
-                processGB(responses)
+                print "self.processGB(responses)"
+                self.processGB(responses)
 
-def processGB(responses):
-    cprint = TextFormatter()
-    forge_points_for_level_up = 0
-    invested_forge_points = 0
-    remaining_fps = 0
-    rankings = {}
+            if rc == "BonusService" and rm == "getLimitedBonuses":
+                for item in rd:
+                    if item["type"] == "contribution_boost":
+                        bonus = (float(item["value"])+100)/100
 
-    for r in responses:
-        rc = r.get('requestClass')
-        rm = r.get('requestMethod')
-        rd = r.get('responseData')
+                        print(chr(27) + "[2J")
+                        print "ARC bonus: ", item["value"],"%"
+                        print "Open GB to use calculator"
 
-        if rc == "CityMapService":
-            forge_points_for_level_up = rd[0]["state"]["forge_points_for_level_up"]
-            invested_forge_points = rd[0]["state"]["invested_forge_points"]
-            remaining_fps = forge_points_for_level_up - invested_forge_points
+                        self.ARC_bonus = bonus
+                break
 
-        if rc == "GreatBuildingsService":
-            rankings = rd["rankings"]
+        self.requestIds.remove(requestId)
 
-    print(chr(27) + "[2J")
-    return_data = printSpots(rankings, remaining_fps)
-    print "Remaining FPs to level: ", remaining_fps
+    def processGB(self, responses):
+        if self.ARC_bonus == 0:
+            print "Please refresh FOE to get initial data about ARC bonus"
+            return False
 
-    if len(return_data) > 0:
-        cprint.cfg('g', 'k', 'b')
-        print "INVEST: ", cprint.out(return_data[0])
-        print "REWARD: ", return_data[1]
-    else:
-        cprint.cfg('k', 'r', 'b')
-        print cprint.out('NO PROFIT HERE :(')
+        cprint = TextFormatter()
+        forge_points_for_level_up = 0
+        invested_forge_points = 0
+        remaining_fps = 0
+        rankings = {}
 
-def printSpots(rankings, remaining_fps):
-    cprint = TextFormatter()
+        for r in responses:
+            rc = r.get('requestClass')
+            rm = r.get('requestMethod')
+            rd = r.get('responseData')
 
-    table = []
-    found_what_to_snipe = False
-    return_data = []
+            if rc == "CityMapService":
+                forge_points_for_level_up = rd[0]["state"]["forge_points_for_level_up"]
+                invested_forge_points = rd[0]["state"]["invested_forge_points"]
+                remaining_fps = forge_points_for_level_up - invested_forge_points
 
-    for r in rankings:
-        rank = "-"
-        name = ""
-        forge_points = 0
-        to_lock_a_spot = 0
-        reward = 0
-        profit = 0
-        profitable_spot = 0
+            if rc == "GreatBuildingsService":
+                rankings = rd["rankings"]
 
-        if 'rank' in r.keys():
-            rank = r["rank"]
+        print(chr(27) + "[2J")
+        return_data = self.printSpots(rankings, remaining_fps)
+        print "Remaining FPs to level: ", remaining_fps
 
-        if 'reward' in r.keys():
-            if 'strategy_point_amount' in r["reward"].keys():
-                reward = int(round(float(r["reward"]["strategy_point_amount"])*ARC_bonus))
+        if len(return_data) > 0:
+            cprint.cfg('g', 'k', 'b')
+            print "INVEST: ", cprint.out(return_data[0])
+            print "REWARD: ", return_data[1]
+        else:
+            cprint.cfg('k', 'r', 'b')
+            print cprint.out('NO PROFIT HERE :(')
 
-        name = r["player"]["name"]
+    def printSpots(self, rankings, remaining_fps):
+        cprint = TextFormatter()
 
-        if 'forge_points' in r.keys():
-            forge_points = r["forge_points"]
+        table = []
+        found_what_to_snipe = False
+        return_data = []
 
-        cprint.cfg('w', 'k', 'f')
+        for r in rankings:
+            rank = "-"
+            name = ""
+            forge_points = 0
+            to_lock_a_spot = 0
+            reward = 0
+            profit = 0
+            profitable_spot = 0
 
-        if forge_points >= remaining_fps and rank != '-':
-            cprint.cfg('r', 'k', 'b')
+            if 'rank' in r.keys():
+                rank = r["rank"]
 
-        if found_what_to_snipe == False and rank < 6 and forge_points < remaining_fps:
-            to_lock_a_spot = int(round(((float(remaining_fps) - forge_points) / 2) + forge_points))
-            profit = reward-to_lock_a_spot
+            if 'reward' in r.keys():
+                if 'strategy_point_amount' in r["reward"].keys():
+                    reward = int(round(float(r["reward"]["strategy_point_amount"])*self.ARC_bonus))
 
-            #if profit + to_lock_a_spot > remaining_fps:
-            #    profit = remaining_fps-to_lock_a_spot
+            name = r["player"]["name"]
 
-            found_what_to_snipe = True
-            if profit > 0:
-                cprint.cfg('g', 'k', 'x')
-                return_data = [to_lock_a_spot, profit, rank]
-            else:
+            if 'forge_points' in r.keys():
+                forge_points = r["forge_points"]
+
+            cprint.cfg('w', 'k', 'f')
+
+            if forge_points >= remaining_fps and rank != '-':
                 cprint.cfg('r', 'k', 'b')
 
-        if rank == '-':
-            cprint.cfg('y', 'k', 'f')
+            if found_what_to_snipe == False and rank < 6 and forge_points < remaining_fps:
+                to_lock_a_spot = int(round(((float(remaining_fps) - forge_points) / 2) + forge_points))
+                profit = reward-to_lock_a_spot
 
-        if rank < 6:
-            rank = cprint.out(rank)
-            name = cprint.out(name)
-            forge_points = cprint.out(forge_points)
-            to_lock_a_spot = cprint.out(to_lock_a_spot)
-            profit = cprint.out(profit)
-            reward = cprint.out(reward)
+                found_what_to_snipe = True
+                if profit > 0:
+                    cprint.cfg('g', 'k', 'x')
+                    return_data = [to_lock_a_spot, profit, rank]
+                else:
+                    cprint.cfg('r', 'k', 'b')
 
-            #table.append([rank, name, forge_points, to_lock_a_spot, reward, profit])
-            table.append([rank, to_lock_a_spot, profit])
+            if rank == '-':
+                cprint.cfg('y', 'k', 'f')
 
-    #TableIt.printTable(table)
-    print(tabulate(table, headers=['#', 'Cost', 'Difference']))
-    return return_data
+            if rank < 6:
+                rank = cprint.out(rank)
+                name = cprint.out(name)
+                forge_points = cprint.out(forge_points)
+                to_lock_a_spot = cprint.out(to_lock_a_spot)
+                profit = cprint.out(profit)
+                reward = cprint.out(reward)
 
+                #table.append([rank, name, forge_points, to_lock_a_spot, reward, profit])
+                table.append([rank, to_lock_a_spot, profit])
+
+        print(tabulate(table, headers=['#', 'Cost', 'Difference']))
+        return return_data
 
 def main():
-    browser = pychrome.Browser(url='http://127.0.0.1:9222')
-    tabs = browser.list_tab()
-   
-    for tab in tabs:
-        tab.start()
-        node = tab.DOM.getDocument()
-        url = node['root']['documentURL']     
+    init_FOE = True
+    tabs = False
+
+    show_msg_1 = True
+
+    print(chr(27) + "[2J")
+    print "Open Chrome with remote debugger or run 'python open_chrome.py'"
+    print "Then open FOE in that window"
+
+    
+    while init_FOE:
+        browser = pychrome.Browser(url='http://127.0.0.1:9222')
         
-        world, domain = url[8:].split('.', 1)
+        try:
+            tabs = browser.list_tab()
+        except:
+            return False
 
-        if 'forgeofempires.com/game' in url:
-            eh = EventHandler(tab, world)
-            tab.Network.responseReceived = eh.handleResponse
-            tab.Network.loadingFinished = eh.handleLoading
-            tab.Network.requestWillBeSent = eh.handleRequest
+        for tab in browser.list_tab():
+            tab.start()
+            node = tab.DOM.getDocument()
+            url = node['root']['documentURL']     
+            
+            world, domain = url[8:].split('.', 1)
 
-            tab.Network.enable()
+            if 'forgeofempires.com/game' in url:
+                eh = EventHandler(tab, world)
+                tab.Network.loadingFinished = eh.handleLoading
+                tab.Network.requestWillBeSent = eh.handleRequest
+
+                tab.Network.enable()
+
+                init_FOE = False
+                print(chr(27) + "[2J")
+                if eh.ARC_bonus == 0:
+                    print "Please refresh FOE to get initial data about ARC bonus"
+                else:
+                    print "Open GB"
         
 if __name__ == '__main__':
     main()
